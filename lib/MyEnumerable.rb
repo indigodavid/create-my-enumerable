66df8f822268db37799b1e040563e165afb21088
David Vera Castillo <indigodavid@gmail.com>
2022-08-19 10:33:59 -0500

    Add any? method to MyEnumerable

--- lib/MyEnumerable.rb
+++ lib/MyEnumerable.rb
@@ -9,5 +9,17 @@ module MyEnumerable
     end
     condition
   end
+
+  def any?
+    # any method
+    condition = false
+    index = 0
+    while index < @list.length
+      condition ||= yield @list[index]
+      break if condition
   
+      index += 1
+    end
+    condition
+  end
 end
